661225aefdc46bec755815eeba6be2bcd206cffe
dkeshri <67820749+dkeshri@users.noreply.github.com>
2022-09-08 21:52:37 +0530

    Create IISCertificates.cs
    IIS certifivctes

---
+++ ConsoleApp/Programming/IISCertificates.cs
@@ -0,0 +1,46 @@
+using System;
+using DesignPrincipal.Basic.Interface;
+
+namespace ConsoleApp.Programming{
+    public class IISCertificates : IExecute
+    {
+        private void GetInstalledIISCretificates(){
+           List<X509Certificate2> certificates = new List<X509Certificate2>();
+
+        X509Store store1 = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+          try
+          {
+              store1.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+
+              foreach (var certificate in store1.Certificates)
+              {
+                  foreach (var extension in certificate.Extensions)
+                  {
+                      if (extension.Oid.FriendlyName == "Enhanced Key Usage" && extension is X509EnhancedKeyUsageExtension enhancedKeyUsageExtension)
+                      {
+                          foreach (var item in enhancedKeyUsageExtension.EnhancedKeyUsages)
+                          {
+                              if (item.FriendlyName == "Server Authentication")
+                              {
+                                  certificates.Add(certificate);
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+          finally
+          {
+              store1.Close();
+          }
+      }
+        
+        
+        
+        public void run()
+        {
+            //fibonacciSerise(8);
+            SeriseOfArmstrongNumber(1000);
+        }
+    }
+}
